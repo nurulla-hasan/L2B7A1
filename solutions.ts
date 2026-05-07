eb585dc55eb580bd4d8d6d75437284eb1392f6cc
nurulla-hasan <nurullahasan.dev@gmail.com>
2026-05-07 12:31:44 +0600

    Solve TypeScript assignment problems

--- solutions.ts
+++ solutions.ts
@@ -3,16 +3,59 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
   return numbers.filter((number) => number % 2 === 0);
 };
 
-const numArray = [1, 2, 3, 4, 5, 6, 43, 38, 46, 453, 234];
-console.log(filterEvenNumbers(numArray));
-
-
-
-
 // solution 2
 const reverseString = (str: string): string => {
   return str.split("").reverse().join("");
 };
 
-const testString = "hello from TypeScript";
-console.log(reverseString(testString));
+// solution 3
+const checkType = (value: string | number): string => {
+  if (typeof value === "string") {
+    return "String";
+  }
+  return "Number";
+};
+
+// solution 4
+const getProperty = <O, K extends keyof O>(obj: O, key: K): O[K] => {
+  return obj[key];
+};
+
+// solution 5
+interface IBook {
+  title: string;
+  author: string;
+  publishedYear: number;
+}
+const toggleReadStatus = (book: IBook): IBook & { isRead: boolean } => {
+  const newObj = { ...book, isRead: true };
+  return newObj;
+};
+
+// solution 6
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+class Student extends Person {
+  grade: string;
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+// solution 7
+const getIntersection = (arr1: number[], arr2: number[]): number[] => {
+  const set = new Set(arr2);
+  return arr1.filter((item) => set.has(item));
+};
